7f12992173b2b2678ab98ce3452dc5cc53ca4bc0
ArchyCillp <11610809@mail.sustc.edu.cn>
2017-12-26 04:20:57 +0800

    update again in 17/12/26

--- task1222/task1222/SPOJ7408_CAMELOT.cpp
+++ task1222/task1222/SPOJ7408_CAMELOT.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-char[60] desk;
-int [12] onland;
+char desk[60];
+int  onland[12];
 int main(){
     while(1){
+        bool ok=true;
         scanf("%s",desk);
         if (desk[0]=='*') break;
         int citizen=0,palace=16;
@@ -23,11 +24,34 @@ int main(){
                     ++onland[desk[i]-'0'];
                 }
             }
-            if (citizen==palace){
-
+            if (citizen==palace || i==51){
+                if (onland[0]>0){
+                    citizen-=onland[0];
+                    onland[0]=0;
+                }
+                if (onland[5]>1){
+                    int mns=onland[5]/2*2;
+                    citizen-=mns;
+                    onland[5]-=mns;
+                }
+                for (int j=1;j<=4;j++){
+                    int mns=onland[j]<onland[10-j]?onland[j]:onland[10-j];
+                    citizen-=(mns*2);
+                    onland[j]-=mns;
+                    onland[10-j]-=mns;
+                }
+                if (citizen==palace){
+                    ok=false;
+                    break;
+                }
             }
         }
 
+        if (!(palace==4 && citizen==0)){
+            ok=false;
+        }
+        if (ok) printf("Y\n");
+        else printf("N\n");
     }
     return 0;
 }
